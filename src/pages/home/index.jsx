32679aa2d7dab1bb527c714f9d73d8f2b81c3284
Gimhan Rodrigo <gimhanrg@gmail.com>
2023-11-19 18:05:14 +0530

    first commit

---
+++ src/pages/home/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import HomeComponent from '../../components/home';
+
+const HomePage = () => {
+  return (
+    <div>
+      <HomeComponent />
+    </div>
+  );
+};
+
+export default HomePage;
